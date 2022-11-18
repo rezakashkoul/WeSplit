04c02d78cb013b7ae4861a267e140e3e3671972d
Reza Kashkoul <rezakashkoul@gmail.com>
2022-11-19 02:28:26 +0330

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Reza Kashkoul on 28-Aban-1401 .
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
